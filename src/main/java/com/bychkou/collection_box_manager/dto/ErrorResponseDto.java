94374854353a485028ba89c90fc7460071104699
Mikita Bychkou <nikitoscoin@gmail.com>
2025-05-14 21:17:06 +0200

    Modified: ErrorResponseDto

--- src/main/java/com/bychkou/collection_box_manager/dto/ErrorResponseDto.java
+++ src/main/java/com/bychkou/collection_box_manager/dto/ErrorResponseDto.java
@@ -1,11 +1,15 @@
 package com.bychkou.collection_box_manager.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class ErrorResponseDto {
 	private String errorMessage;
